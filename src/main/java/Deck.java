e6f9483b3135abe6501da9a817d0fb9795073c4b
Morgaine <morgainedv@gmail.com>
2020-01-09 14:57:17 +0000

    adds populateDeck method

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -11,4 +11,13 @@ public class Deck {
     public int countCards() {
         return cards.size();
     }
+
+    public void populateDeck() {
+        for (SuitType suit : SuitType.values()) {
+            for (RankType rank : RankType.values()) {
+                Card card = new Card(suit,rank);
+                cards.add(card);
+            }
+        }
+    }
 }
