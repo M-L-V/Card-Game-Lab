d3db1d4e73ffa00726b96ace75a6cdc5e547ab34
Morgaine <morgainedv@gmail.com>
2020-01-09 14:40:27 +0000

    adds deckstartingempty test

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> cards;
+
+    public Deck(){
+        this.cards = new ArrayList<Card>();
+    }
+
+    public int countCards() {
+        return cards.size();
+    }
 }
