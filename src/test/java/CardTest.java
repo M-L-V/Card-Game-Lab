41ef21321e42e47df67179652f9be1f660ed6212
Morgaine <morgainedv@gmail.com>
2020-01-09 13:52:06 +0000

    adds getsSuit method

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CardTest {
+
+    Card card;
+
+
+    @Before
+    public void before(){
+        card = new Card(SuitType.SPADES, RankType.ACE);
+    }
+
+    @Test
+    public void canGetSuit(){
+        assertEquals(SuitType.SPADES, card.getSuit());
+    }
+
 }
